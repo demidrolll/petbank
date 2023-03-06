0eb1518b3ae9c94e6dc517ef98f288d485839666
dmitry.ivzhenko <mrdemidrolll@gmail.com>
2023-03-06 20:32:56 +0300

    feat(authserver): first commit

---
+++ authserver/src/test/java/com/demidrolll/pet/bank/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demidrolll.pet.bank.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
